5f4abf25a14fa7527d05f4219c99cf00677ddcbe
CoopLo <cooper.lorsung@gmail.com>
2019-11-26 01:35:47 -0500

    Updated so the projected target isn't all over the place. This implementation works for the current parameters. It seems like the parameters need to be tuned for each case in order to get a successful interception. Assumes intercepting missile blast radius is 5m. Needs to be refactored

--- tracking_missile.py
+++ tracking_missile.py
@@ -8,9 +8,10 @@ from mpl_toolkits.mplot3d import Axes3D
 def inside_hemisphere(x, y, z, center, radius):
     return np.sqrt((x-center[0])**2 + (y-center[1])**2 + z**2) < radius
 
-def plot(steps, i, x, y, z, x_up, y_up, z_up, kt, center, radius, inside):
+def plot(steps, i, x, y, z, x_up, y_up, z_up, kt, center, radius, inside,
+         proj_x, proj_y, proj_z, dome_x, dome_y, dome_z, r_x, r_y, r_z, done=False):
     color = 'r' if(i <= kt) else 'b'
-    label = "THRUST" if(i <= kt) else "NO THRUST"
+    label = "ENEMY" if(i <= kt) else "NO THRUST"
     dome_color = 'g' if(inside) else 'b'
 
     leading_zeros = int(np.log(steps)/np.log(10))
@@ -20,20 +21,26 @@ def plot(steps, i, x, y, z, x_up, y_up, z_up, kt, center, radius, inside):
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot rocket
-    ax.scatter(x, y, z, color=color, label=label)
+    s = 0 if(done) else 20
+    ax.scatter(x, y, z, color=color, label=label, s=s)
 
     # Plot detection hemisphere
-    r = 500
-    phi, theta = np.mgrid[0.0:0.5*np.pi:180j, 0.0:2.0*np.pi:720j] # phi = alti, theta = azi
-    x = radius*np.sin(phi)*np.cos(theta) + center[0]
-    y = radius*np.sin(phi)*np.sin(theta) + center[1]
-    z = radius*np.cos(phi)
-    ax.plot_surface(x, y, z, alpha=0.3, color=dome_color)
-    ax.scatter(*center, color='k')
+    ax.plot_surface(dome_x, dome_y, dome_z, alpha=0.3, color=dome_color)
+    ax.scatter(*center, color='k', label="SENSOR")
 
-    ax.set(xlim=(-0.5, x_up), ylim=(-0.5, y_up), zlim=(-0.5, 500), #zlim=(-0.5, z_up),
+    # Plot position projection
+    ax.scatter(proj_x, proj_y, proj_z, color='k', marker='x', label="POSITION PROJECTION")
+
+    # Plot response rocket
+    s = 100 if(done) else 20
+    label = "SUCCESSFUL INTERCEPTION" if(done) else "RESPONSE"
+    ax.scatter(r_x, r_y, r_z, color='g', label=label, s=s)
+
+    x_up = max(x_up, center[0]+radius)
+    y_up = max(y_up, center[0]+radius)
+    ax.set(xlim=(-0.5, x_up), ylim=(-0.5, y_up), zlim=(-0.5, z_up), #zlim=(-0.5, 500),
            xlabel="x", ylabel="y", zlabel="z")
-    ax.legend(loc='best')
+    ax.legend(loc='upper left')
     plt.savefig("./track_graph_missile/{}{}.png".format(zeros,i))
     plt.close(fig=fig)
     #exit(1)
@@ -44,12 +51,19 @@ def kinematics(vx0, vy0, vz0, t, x0=0, y0=0, z0=0, g=0, vt=0):
     # from http://farside.ph.utexas.edu/teaching/336k/Newtonhtml/node29.html
     vx = vx0 + vx0*np.exp(-g*t/vt)
     vy = vy0 + vy0*np.exp(-g*t/vt)
-    vz = vz0 + vz0*np.exp(-g*t/vt) - vt*(1- np.exp(-g*t/vt)) # Height dependent drag
+    vz = vz0 + vz0*np.exp(-g*t/vt) - vt*(1- np.exp(-g*t/vt)) # NEED Height dependent drag
 
     x = x0 + vx0*vt/g*(1 - np.exp(-g*t/vt))
     y = y0 + vy0*vt/g*(1 - np.exp(-g*t/vt))
     z = z0 + vt/g*(vz0 + vt)*(1 - np.exp(-g*t/vt)) - vt*t
     return x, y, z, vx, vy, vz
+
+
+def kinematics_acc(vx0, vy0, vz0, ax, ay, az, t, x0=0, y0=0, z0=0, g=0, vt=0):
+    x = x0 + vx0*vt/g*(1 - np.exp(-g*t/vt))
+    y = y0 + vy0*vt/g*(1 - np.exp(-g*t/vt))
+    z = z0 + vt/g*(vz0 + vt)*(1 - np.exp(-g*t/vt)) - vt*t
+    return x, y, z
 
 
 @jit
@@ -99,7 +113,7 @@ def rocket():
     z = 0
 
     C_d = 0.1            # Coefficient of drag
-    c = 50000             # Exhaust force
+    c = 70000             # Exhaust force
     delta_m = -1         # Change in mass
     A = 0.25              # Cross sectional area
     mass = 1000           # Total mass of rocket without fuel
@@ -123,29 +137,43 @@ def rocket():
 
 
     # Intercepting system variables
+    #time_projection = 115
     center = [1000, 1000]
     radius = 500
-    inside = []
-    vx = 0
-    vy = 0
-    vz = 0
-    xs = [center[0]]
-    ys = [center[1]]
-    zs = [0]
-    target_x = []
-    target_y = []
-    target_z = []
+    inside = [False]
+    r_vx = 0
+    r_vy = 0
+    r_vz = 0
+    r_x = center[0]
+    r_y = center[1]
+    r_z = 0
+    r_xs = [center[0]]
+    r_ys = [center[1]]
+    r_zs = [0]
+    r_vxs = [0]
+    r_vys = [0]
+    r_vzs = [0]
     evx = []
     evy = []
     evz = []
+    target_x = []
+    target_y = []
+    target_z = []
     estimation = []
+    proj_x = [center[0]]
+    proj_y = [center[1]]
+    proj_z = [0]
+    cur_dists = []
 
-    C_d = 0.1            # Coefficient of drag
-    r_c = 300000           # Exhaust force
+    r_C_d = 0.1            # Coefficient of drag
+    r_c = 100000           # Exhaust force
+    projection = 115
+    time_projection = 115
     r_delta_m = -1         # Change in mass
-    r_A = 0.25              # Cross sectional area
-    r_mass = 1000           # Total mass of rocket without fuel
-    r_r_mass = 1000         # Mass of fuel
+    r_A = 0.25             # Cross sectional area
+    r_mass = 1000          # Total mass of rocket without fuel
+    r_r_mass = 1000        # Mass of fuel
+    current_dist = 500
 
 
     rho_0 = 1.2754        # Initial density of air
@@ -196,15 +224,18 @@ def rocket():
 
             # Estimate new velocity and acceleration for enemy missile
             if(len(target_x) > 3):
+                # Adjust estimation parameters
                 estimation.append(i)
                 target_x = target_x[1:]
                 target_y = target_y[1:]
                 target_z = target_z[1:]
 
+                # Calculate new x velocities
                 x_vel = (target_x[-1] - target_x[-2])/delta_t
                 y_vel = (target_y[-1] - target_y[-2])/delta_t
                 z_vel = (target_z[-1] - target_z[-2])/delta_t
 
+                # Calculate new accelerations
                 x_acc = (target_x[0] - 2*target_x[1] + target_x[2])/(delta_t**2)
                 y_acc = (target_y[0] - 2*target_y[1] + target_y[2])/(delta_t**2)
                 z_acc = (target_z[0] - 2*target_z[1] + target_z[2])/(delta_t**2)
@@ -212,9 +243,84 @@ def rocket():
                 evx.append(x_vel)
                 evy.append(y_vel)
                 evz.append(z_vel)
+
+                # Project target point
+                px, py, pz, = kinematics_acc(x_vel, y_vel, z_vel, x_acc, y_acc, z_acc,
+                                    time_projection*delta_t,
+                                    target_x[-1], target_y[-1], target_z[-1], g, vt)
                 
-                # MAYBE TARGET POINT CLOSER AND CLOSER TO ENEMY AS WE APPROACH?
-                # ASSUMING ALL PERFECT KNOWLEDGE OF EVERYTHING
+                proj_x.append(px)
+                proj_y.append(py)
+                proj_z.append(pz)
+
+                # Aim response missile at target point
+                cur_dists.append(current_dist)
+
+                dist_vec = [(px-r_x), (py - r_y), (pz - r_z)]
+                #dist_vec = [(target_x[-1]-r_x), (target_y[-1] - r_y), (target_z[-1] - r_z)]
+                #print(dist_vec)
+                azimuth = np.arccos(dist_vec[2]/np.linalg.norm(dist_vec))
+                radial = np.arctan(dist_vec[1]/dist_vec[0])
+
+                x_frac = np.sin(azimuth)*np.cos(radial)
+                y_frac = np.sin(azimuth)*np.sin(radial)
+                z_frac = np.cos(azimuth)
+                normalize = x_frac + y_frac + z_frac
+
+                x_frac /= normalize
+                y_frac /= normalize
+                z_frac /= normalize
+
+                x_frac = dist_vec[0]/np.linalg.norm(dist_vec)
+                y_frac = dist_vec[1]/np.linalg.norm(dist_vec)
+                z_frac = dist_vec[2]/np.linalg.norm(dist_vec)
+
+                #print("X: {}, Y: {}, Z: {}\n\n".format(x_frac, y_frac, z_frac))
+
+                r_x, r_y, r_z, r_vx, r_vy, r_vz, r_r_mass = rocket_equation(r_vx, r_vy, r_vz, 
+                                            (i-estimation[0])*delta_t,
+                                            r_x, r_y, r_z,
+                                            g, rho_0, r_mass, r_r_mass, delta_t, r_delta_m, 
+                                            r_A, r_C_d, r_c, x_frac, y_frac, z_frac)
+
+                current_dist = np.sqrt((target_x[-1] - r_x)**2 + 
+                                       (target_y[-1] - r_y)**2 + 
+                                       (target_z[-1] - r_z)**2)
+                time_projection = projection*(current_dist/radius)
+                if(r_vz < 0):
+                    r_vz = 0
+                if(current_dist < 10):
+                    r_vx = 0
+                    r_vy = 0
+                    r_vz = 0
+
+                r_xs.append(r_x)
+                r_ys.append(r_y)
+                r_zs.append(r_z)
+                r_vxs.append(r_vx)
+                r_vys.append(r_vy)
+                r_vzs.append(r_vz)
+
+            else:
+                proj_x.append(1000)
+                proj_y.append(1000)
+                proj_z.append(0)
+                r_xs.append(1000)
+                r_ys.append(1000)
+                r_zs.append(0)
+                r_vxs.append(0)
+                r_vys.append(0)
+                r_vzs.append(0)
+        else:
+            proj_x.append(1000)
+            proj_y.append(1000)
+            proj_z.append(0)
+            r_xs.append(1000)
+            r_ys.append(1000)
+            r_zs.append(0)
+            r_vxs.append(0)
+            r_vys.append(0)
+            r_vzs.append(0)
 
 
         # Hold on to current values
@@ -224,19 +330,26 @@ def rocket():
         vxs.append(vx)
         vys.append(vy)
         vzs.append(vz)
+
         if(z < 1e-2 and i>100):
+            # Match dimensions
+            proj_x.append(proj_x[-1])
+            proj_y.append(proj_y[-1])
+            proj_z.append(proj_z[-1])
+            inside.append(inside[-1])
             break
+
+        try:
+            if(current_dist <= 5):
+                break
+        except UnboundLocalError:
+            pass
 
     end = True
     maxx = []
     maxy = []
     maxz = []
     fig, ax = plt.subplots()
-    #ax.plot(vxs)
-    #ax.plot(vys)
-    #ax.plot(vzs)
-    #plt.show()
-    #exit(1)
 
     for i in range(len(xs)):
         maxx.append(np.max(xs[i]))
@@ -244,10 +357,22 @@ def rocket():
         maxz.append(np.max(zs[i]))
 
     kt = 1000
-    #for i in tqdm(range(0, len(xs))):
-    #    plot(steps, i, xs[i], ys[i], zs[i],
-    #         1.1*np.max(maxx), 1.1*np.max(maxy), 1.1*np.max(maxz), kt,
-    #         center, radius, inside[i])
+    phi, theta = np.mgrid[0.0:0.5*np.pi:180j, 0.0:2.0*np.pi:720j] # phi = alti, theta = azi
+    dome_x = radius*np.sin(phi)*np.cos(theta) + center[0]
+    dome_y = radius*np.sin(phi)*np.sin(theta) + center[1]
+    dome_z = radius*np.cos(phi)
+    for i in tqdm(range(0, len(xs))):
+        plot(steps, i, xs[i], ys[i], zs[i],
+             1.1*np.max(maxx), 1.1*np.max(maxy), 1.1*np.max(maxz), kt,
+             center, radius, inside[i], proj_x[i], proj_y[i], proj_z[i], dome_x, dome_y, dome_z,
+             r_xs[i], r_ys[i], r_zs[i])
+
+    for i in tqdm(range(0, 20)):
+        plot(steps, i+len(xs), xs[-1], ys[-1], zs[-1],
+             1.1*np.max(maxx), 1.1*np.max(maxy), 1.1*np.max(maxz), kt,
+             center, radius, inside[-1], proj_x[-1], proj_y[-1], proj_z[-1],
+             dome_x, dome_y, dome_z,
+             r_xs[-1], r_ys[-1], r_zs[-1], done=True)
 
     print("WRITING FILE")
     np.savetxt("./track_graph_missile/x_dat.txt", xs)
@@ -256,6 +381,15 @@ def rocket():
     np.savetxt("./track_graph_missile/x_vel.txt", vxs)
     np.savetxt("./track_graph_missile/y_vel.txt", vys)
     np.savetxt("./track_graph_missile/z_vel.txt", vzs)
+    np.savetxt("./track_graph_missile/proj_x.txt", proj_x)
+    np.savetxt("./track_graph_missile/proj_x.txt", proj_y)
+    np.savetxt("./track_graph_missile/proj_x.txt", proj_z)
+    np.savetxt("./track_graph_missile/r_x_dat.txt", r_xs)
+    np.savetxt("./track_graph_missile/r_y_dat.txt", r_ys)
+    np.savetxt("./track_graph_missile/r_z_dat.txt", r_zs)
+    np.savetxt("./track_graph_missile/r_x_vel.txt", r_vxs)
+    np.savetxt("./track_graph_missile/r_y_vel.txt", r_vys)
+    np.savetxt("./track_graph_missile/r_z_vel.txt", r_vzs)
     fig, ax = plt.subplots()
     ax.plot(vxs, label="X VELOCITY")
     ax.plot(vys, label="Y VELOCITY")
@@ -266,9 +400,8 @@ def rocket():
     ax.plot(estimation, evz, label="ESTIMATED Z VELOCITY")
 
     ax.legend(loc='best')
+    os.system("cd ./track_graph_missile/ ; ffmpeg -pattern_type glob -i \"[0-9]*.png\" -c:v libx264 -pix_fmt yuv420p -movflags +faststart output.mp4")
     plt.savefig("./track_graph_missile/velocities.png")
-    #os.system("cd ./track_graph_missile/ ; ffmpeg -pattern_type glob -i \"*.png\" -c:v libx264 -pix_fmt yuv420p -movflags +faststart output.mp4")
-    #plt.show()
 
 
 if __name__ == '__main__':
